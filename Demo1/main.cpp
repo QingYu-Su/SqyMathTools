47414e5f5f3f6ee9af39f7549f99253de3fb29fd
青羽 <@5ACKJ3FF3NS95NR>
2023-02-16 23:38:57 +0800

    计算器必须加上负数，否则无法计算

--- Demo1/main.cpp
+++ Demo1/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <Windows.h>
-#include "MathFunction.h"
-#pragma comment(lib, "../x64/Debug/SqyMathLibrary.lib")
+#include "./include/MathFunction.h"
 using namespace SqyMathLibrary;
 using namespace std;
 
@@ -10,13 +9,16 @@ int main() {
 	MathExpression a{ "3", "+", "10" };
 	cout << clac -> Calculate(a);*/
 	MathExpression a{ "3", "+", "x"};
-	FunctionExpression b(a, 'y', 'x', -10, 10);
+	FunctionExpression b(a, 'y', 'x', 5, 20);
 	/*FunctionTool k;
 	cout << k.GetValue(b, 11) << endl;*/
-	NormalFunction c(b);
-	/*FunctionMap d = c->Calculate(-10, 10, 1);
+	MathFunction *c = new NormalFunction(b);
+	FunctionMap d = c->Calculate(10, 15, 10);
+	if (c->IsSuccess() == false) {
+		cout << c->GetError();
+	}
 	for (int i = 0; i < d.size(); i++) {
 		cout << d[i].first << " " << d[i].second << endl;
-	}*/
+	}
 	return 0;
 }
